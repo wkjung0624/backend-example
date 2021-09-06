a3da8ca58963874748b279469af6f8f20426e3d6
wkjung0624 <skyship36@gmail.com>
2021-09-06 23:13:41 +0900

    Update example.js

--- example.js
+++ example.js
@@ -1,11 +1,12 @@
 /*
 npm i axios cors express express-session connect-redis 
-
 https://redis.io/commands/command
 */
+
+const fs = require('fs');
 const cors = require('cors');
 const redis = require('redis');
-const axios = require('axios');
+// const axios = require('axios');
 const session = require('express-session');
 const connectRedis = require('connect-redis');
 const RedisStore = connectRedis(session);
@@ -31,11 +32,16 @@ const app = express();
 //     console.log('redis connect')
 // });
 
-let http = require('http');
-const {
-    client
-} = require('./redis.js');
-let server = http.createServer(app);
+const http = require('http');
+const server = http.createServer(app);
+
+let sql = require('./sql.js');
+
+fs.watchFile(__dirname + '/sql.js', (curr, prev) => {
+    console.log('sql 변경시 재시작 없이 반영되도록 함.');
+    delete require.cache[require.resolve('./sql.js')];
+    sql = require('./sql.js');
+  });
 
 app.use(express.json());
 app.use(cors());
@@ -50,7 +56,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         secure: false,
-        maxAge: 24 * 60 * 60 // 쿠키 유효시간 1시간
+        maxAge: 1000 * 60 * 60 // 쿠키 유효시간 1시간
     },
     store: new RedisStore({
         client: redis,
@@ -78,16 +84,14 @@ app.get('/api', async (req, res) => {
     console.log(req.query.key);
     console.log(req.query.value);
     //res.send(await req.db(request.params.alias, request.body.param, request.body.where));
-    redisClient.set(req.query.key, req.query.value, (err, reply) => {
-
-    })
+    redisClient.set(req.query.key, req.query.value);
     redisClient.get(req.query.key, (err, reply) => {
-        // if (err) throw err;
         console.log(reply);
     })
 
     res.status(404).send('Not found');
 });
+
 
 server.listen(5000, function () {
     console.log('Express server listening on port ' + server.address().port);
