a88fa2c198dec98a67fc2049ddad372cf0c01a8e
wkjung0624 <skyship36@gmail.com>
2021-09-10 22:38:48 +0900

    refactor: Separate files from app.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,10 +1,28 @@
-const express = require('express');
-const user = require('../models/users');
-const view = ''
+const passport = require('../models/passport');
 
 module.exports = {
-    // router.js 에서 사용하는 함수를 여기에 정의함
-    goToRoot: (req, res, next) => {
-        console.log("test");
-    }
+    getAuthenticate: passport.authenticate('kakao'),
+    authCallBack : passport.authenticate('kakao', 
+        {
+            successRedirect: '/success',
+            failureRedirect : '/fail',
+        }
+    ),
+    logout: (req, res) => {
+        req.logout();
+        req.session.destroy(function(err){
+            res.redirect('/');
+        })
+    },
+    success : (req, res) => { res.send("success") },
+    fail : (req, res) => { res.send("fail") },
+    f1 : (req, res) => { 
+        // req 의 user는 passport에서 인젝션해줘서 생기는 객체, 예약어
+        console.log(req.user,2)
+        res.send("f1") 
+    },
+    f2 : (req, res) => { 
+        console.log(req.user,3)
+        res.send("f2") 
+    },
 }
